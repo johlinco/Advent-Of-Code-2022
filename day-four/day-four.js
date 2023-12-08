bcbba12aa79cf52915eb397b8d6bdaafbd7cd25b
Lincoln Hoey Moore <johlinco@gmail.com>
2023-12-08 04:05:49 -0500

    Update day-four.js

--- day-four/day-four.js
+++ day-four/day-four.js
@@ -1,5 +1,35 @@
 const fs = require("fs");
 
 const input = fs.readFileSync("day-four/input.txt", "utf8");
+const example = fs.readFileSync("day-four/example.txt", "utf8");
 
-let rucksacks = input.split(/\r?\n/)
+function countEnveloppingSets(sets) {
+    let count = 0
+    sets = sets.split(/\r?\n/)
+    console.log(sets)
+
+    for (let set of sets) {
+        set = set.replace(/-/g, ' ')
+        set = set.split(",")
+        let firstFirst = parseInt(set[0][0])
+        let firstLast = parseInt(set[0][2])
+        let lastFirst = parseInt(set[1][0])
+        let lastLast = parseInt(set[1][2])
+        console.log(firstFirst, firstLast, lastFirst, lastLast)
+        if (firstFirst >= lastFirst && firstLast <= lastLast) {
+            console.log(firstFirst, firstFirst, lastFirst, lastLast)
+            console.log('here)')
+            count++
+        }
+        if (lastFirst >= firstFirst && lastLast <= firstLast) {
+            console.log(firstFirst, firstFirst, lastFirst, lastLast)
+            console.log('here')
+            count++
+        }
+    }
+   
+    return count
+}
+
+console.log(countEnveloppingSets(example))
+console.log(countEnveloppingSets(input))
