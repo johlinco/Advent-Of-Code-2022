d313f91f1e568c98d91e778fc2cfa965001b603e
Lincoln Hoey Moore <johlinco@gmail.com>
2024-05-14 14:07:19 -0400

    So close and yet so far
    Still unsure about why I'm getting wrong answer

--- day-seven/day-seven.js
+++ day-seven/day-seven.js
@@ -8,38 +8,40 @@ const exampleRows = example.split(/\r?\n/)
 const inputRows = input.split(/\r?\n/)
 
 class Directory {
-    constructor() {
+    constructor(name) {
         this.children = [];
         this.size = 0;
-        this.parent = ""
+        this.parent = "";
+        this.name = name;
     }
 }
 
 function sumOfDirsUnderOneHundredK(commands) {
-    let dirStack = [];
     let currDir = "/";
-    let root = new Directory("/")
+    let root = new Directory("/", "")
     let dirTree = {};
  
     dirTree["/"] = root
-    dirStack.push(currDir);
-
+    console.log(dirTree["/"])
     for (const command of commands) {
         let commandArray = command.split(" ")
+       // console.log(dirTree[currDir], commandArray)
         if (commandArray[0] === "$") {
             if (commandArray[1] === "cd") {
+                
                 if (commandArray[2] === "..") {
-                    dirStack.pop();
-                    currDir = dirStack[dirStack.length - 1];
+                    if (currDirr = "/") continue;
+                    currDir = dirTree[currDir].parent
+                } else if (commandArray[2] === "/") {
+                    currDir = "/"
                 } else {
-                    dirStack.push(commandArray[2]);
                     currDir = commandArray[2];
                 }
             }
         } else if (commandArray[0] === "dir") {
             let dirName = commandArray[1];
             if (!dirTree[dirName]) {
-                let dir = new Directory()
+                let dir = new Directory(dirName)
                 dirTree[dirName] = dir;
                 dirTree[dirName].parent = currDir;
                 dirTree[currDir].children.push(dirName);
@@ -57,12 +59,12 @@ function sumOfDirsUnderOneHundredK(commands) {
     let underOneHundredKSum = 0
 
     function dfs(root) {
+        console.log(root)
         if (!root) return
         if (root.size <= 100000) {
             underOneHundredKSum += root.size
         }
         for (const child of root.children) {
-            console.log(dirTree[child])
             dfs(dirTree[child])
         }
     }
