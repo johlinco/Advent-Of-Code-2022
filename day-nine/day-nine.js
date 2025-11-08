f674169b8110a9c231a48d8251b0367a08a13b87
Lincoln Hoey Moore <47604064+johlinco@users.noreply.github.com>
2025-11-08 10:02:09 -0500

    part Two

--- day-nine/day-nine.js
+++ day-nine/day-nine.js
@@ -4,8 +4,10 @@ const fs = require("fs");
 
 const input = fs.readFileSync("day-nine/input.txt", "utf8");
 const example = fs.readFileSync("day-nine/example.txt", "utf8");
+const partTwoExample = fs.readFileSync("day-nine/part-two-example.txt", "utf8");
 
 const exampleRows = example.split(/\r?\n/)
+const partTwoExampleRows = partTwoExample.split(/\r?\n/)
 const inputRows = input.split(/\r?\n/)
 
 
@@ -13,13 +15,12 @@ const trailingKnotMover = (hx, hy, tx, ty) => {
         // tail moving
         let yDiff = hy - ty
         let xDiff = hx - tx
-        console.log('xDiff', xDiff, 'yDiff', yDiff)
         // up case hy - ty is 2 and hx - tx is 0
         if (yDiff === 2 &&  xDiff === 0) {
             ty++
         } else if (
             // up right case (hy - ty is 1 AND hx - tx is 2) OR (hy - ty is 2 AND hx - tx is 1)
-            (yDiff === 1 && xDiff === 2) || (yDiff === 2 && xDiff === 1)
+            (yDiff === 1 && xDiff === 2) || (yDiff === 2 && xDiff === 1) || (yDiff === 2 && xDiff === 2)
         ) {
             ty++
             tx++
@@ -30,7 +31,7 @@ const trailingKnotMover = (hx, hy, tx, ty) => {
             tx++
         } else if (
              // down right case (hy - ty is -1 AND hx - tx is 2) OR (hy - ty is -2 AND hx - tx is 1)
-             (yDiff === -1 && xDiff === 2) || (yDiff === -2 && xDiff === 1)
+             (yDiff === -1 && xDiff === 2) || (yDiff === -2 && xDiff === 1) || (yDiff === -2 && xDiff === 2)
         ) {
             ty--
             tx++
@@ -41,7 +42,7 @@ const trailingKnotMover = (hx, hy, tx, ty) => {
             ty--
         } else if (
             // down left case (hy - ty is -1 AND hx - tx is -2) OR (hy - ty is -2 AND hx - tx is -1)
-            (yDiff === -1 && xDiff === -2) || (yDiff === -2 && xDiff === -1)
+            (yDiff === -1 && xDiff === -2) || (yDiff === -2 && xDiff === -1) || (yDiff === -2 && xDiff === -2)
         ) {
             ty--
             tx--
@@ -52,8 +53,8 @@ const trailingKnotMover = (hx, hy, tx, ty) => {
             tx--
         } else if (
             // up left case (hy - ty is 1 AND hx - tx is -2) OR (hy - ty is 2 AND hx - tx is -1)
-            (yDiff === 1 && xDiff === -2) || (yDiff === 2 && xDiff === -1)
-        ) { 
+            (yDiff === 1 && xDiff === -2) || (yDiff === 2 && xDiff === -1) || (yDiff === 2 && xDiff === -2)
+        ) {
             tx--
             ty++
         }
@@ -124,29 +125,44 @@ function longTailPositionReporter(directions) {
             } else if (dir === "R") {
                 hx++
             }
-            [oneX, oneY] = trailingKnotMover(hx, hy, oneX, oneY)
-            [twoX, twoY] = trailingKnotMover(oneX, oneY, twoX, twoY)
-            [threeX, threeY] = trailingKnotMover(twoX, twoY, threeX, threeY)
-            [fourX, fourY] = trailingKnotMover(threeX, threeY, fourX, fourY)
-            [fiveX, fiveY] = trailingKnotMover(fourX, fourY, fiveX, fiveY)
-            [sixX, sixY] = trailingKnotMover(fiveX, fiveY, sixX, sixY)
-            [sevenX, sevenY] = trailingKnotMover(sixX, sixY, sevenX, sevenY)
-            [eightX, eightY] = trailingKnotMover(sevenX, sevenY, eightX, eightY)
-            [tx, ty] = trailingKnotMover(eightX, eightY, ty, tx)
-            console.log('head', hx, hy)
-            console.log('one', oneX, oneY)
+            const oneResult = trailingKnotMover(hx, hy, oneX, oneY)
+            oneX = oneResult[0]
+            oneY = oneResult[1]
+            const twoResult = trailingKnotMover(oneX, oneY, twoX, twoY)
+            twoX = twoResult[0]
+            twoY = twoResult[1]
+            const threeResult = trailingKnotMover(twoX, twoY, threeX, threeY)
+            threeX = threeResult[0]
+            threeY = threeResult[1]
+            const fourResult = trailingKnotMover(threeX, threeY, fourX, fourY)
+            fourX = fourResult[0]
+            fourY = fourResult[1]
+            const fiveResult = trailingKnotMover(fourX, fourY, fiveX, fiveY)
+            fiveX = fiveResult[0]
+            fiveY = fiveResult[1]
+            const sixResult = trailingKnotMover(fiveX, fiveY, sixX, sixY)
+            sixX = sixResult[0]
+            sixY = sixResult[1]
+            const sevenResult = trailingKnotMover(sixX, sixY, sevenX, sevenY)
+            sevenX = sevenResult[0]
+            sevenY = sevenResult[1]
+            const eightResult = trailingKnotMover(sevenX, sevenY, eightX, eightY)
+            eightX = eightResult[0]
+            eightY = eightResult[1]
+            const trailingResult = trailingKnotMover(eightX, eightY, tx, ty)
+            tx = trailingResult[0]
+            ty = trailingResult[1]
             tailPositionSet.add(`${tx.toString()},${ty.toString()}`)
         }
     }
-
     return tailPositionSet.size
 }
 
 
-// console.log(tailPositionReporter(exampleRows))
-// console.log(tailPositionReporter(inputRows))
+console.log(tailPositionReporter(exampleRows))
+console.log(tailPositionReporter(inputRows))
 
-console.log(longTailPositionReporter(exampleRows))
-//console.log(longTailPositionReporter(inputRows))
+console.log(longTailPositionReporter(partTwoExampleRows))
+console.log(longTailPositionReporter(inputRows))
 
 
