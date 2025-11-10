2d88491f5e755037dd5bb583ad9ec5e31ad4379e
Lincoln Hoey Moore <47604064+johlinco@users.noreply.github.com>
2025-11-10 17:01:22 -0500

    starting to add it all up

--- day-seven/day-seven.js
+++ day-seven/day-seven.js
@@ -7,6 +7,15 @@ const example = fs.readFileSync("example.txt", "utf8");
 const exampleRows = example.split(/\r?\n/)
 const inputRows = input.split(/\r?\n/)
 
+/**
+ * 
+ * dir = {
+ *      parent: string,
+ *      childrenDirs: array,
+ *      files: array
+ *  }
+ * 
+ */
 
 const fileTreeBuilder = (commands) => {
     let fileTree = {}
@@ -18,11 +27,37 @@ const fileTreeBuilder = (commands) => {
             if (splitCommand[2] === "..") {
                 currDir.pop()
             } else {
+                let parentDir = [...currDir]
                 currDir.push(splitCommand[2])
+                let dirName = currDir.join("/")
+                if (!fileTree[dirName]) {
+                    fileTree[dirName] = {
+                        parent: parentDir.join("/"),
+                        children: [],
+                        files: []
+                    }
+                }
             }
+        } else if (splitCommand[0] === "dir") { 
+            fileTree[currDir.join("/")].children.push(splitCommand[1])
+        } else if (splitCommand[0] !== "$") {
+            fileTree[currDir.join("/")].files.push({
+                name: splitCommand[1],
+                size: parseInt(splitCommand[0])
+            })
         }
-        console.log(splitCommand, currDir)
+        //console.log(splitCommand, currDir, JSON.stringify(fileTree))
     }
+    return fileTree
 }
 
-fileTreeBuilder(exampleRows)
+const dirSizer = (dir) => {
+    const fileTree = fileTreeBuilder(exampleRows)
+    let fileSize = 0
+    for (let i = 0; i < fileTree[dir].files.length; i++) {
+        fileSize += fileTree[dir].files[i].size
+        console.log(fileSize)
+    }
+}
+console.log(fileTreeBuilder(exampleRows))
+dirSizer("/")
